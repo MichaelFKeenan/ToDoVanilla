0a82e75fc1b69bd5f8508bfd081733f5ff89790e
Michael Keenan <Michael.Keenan@howdens.com>
2020-02-20 16:39:35 +0000

    mewssing about with modules

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 var express = require('express');
 var app = express();
+const path = require('path');
+const router = express.Router();
 
-app.use(express.static('public'));
-
-app.get('/', function (req, res) {
-    res.send('An alligator approaches!');
+router.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname + '/public/views/index.html'));
+    const filterBtn = document.getElementById('complete-filter-btn');
+    console.log(filterBtn);
 });
 
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
+router.get('/about', function (req, res) {
+    res.sendFile(path.join(__dirname + '/public/views/about.html'));
 });
+
+app.use(express.static(__dirname + '/public/views'));
+app.use(express.static(__dirname + '/public/scripts'));
+
+app.use('/', router);
+app.listen(process.env.port || 3000);
+
+console.log('Running at Port 3000');
--- public/scripts/toDoList.js
+++ public/scripts/toDoList.js
@@ -1,61 +1,83 @@
-const toDoList = function () {
-    const toDoItems = [
-        {
-            "Name": "item one name",
-            "Complete": true,
-            "Priority": 1
-        },
-        {
-            "Name": "item two name",
-            "Complete": false,
-            "Priority": 3
-        },
-        {
-            "Name": "item three name",
-            "Complete": false,
-            "Priority": 2
-        }
-    ]
 
-    class highPriorityFilter {
-        name = "highPriorityFilter"
-        active = false;
-        filter = (toDoItem) => {
-            return this.active ? toDoItem.Priority >= 3 : true
-        }
+// get these from json file
+// populate on construction/init
+const toDoItems = [
+    {
+        "Name": "item one name",
+        "Complete": true,
+        "Priority": 1
+    },
+    {
+        "Name": "item two name",
+        "Complete": false,
+        "Priority": 3
+    },
+    {
+        "Name": "item three name",
+        "Complete": false,
+        "Priority": 2
     }
+]
 
-    class completeFilter {
-        name = "completeFilter"
-        active = false;
-        filter = (toDoItem) => {
-            return this.active ? !toDoItem.Complete : true
-        }
+// move these
+// implement interface?
+class HighPriorityFilter {
+    name = "highPriorityFilter";
+    htmlIdentifier = "high-priority-filter-btn";
+    active = false;
+    filter = (toDoItem) => {
+        return this.active ? toDoItem.Priority >= 3 : true
     }
+}
 
-    const filters = [new highPriorityFilter(), new completeFilter()]
-
-    const getFilteredItems = () => {
-        const filteredResults = toDoItems.filter(item => {
-            // only do active filters?
-            return filters.every(filter => filter.filter(item));
-        })
-        return filteredResults;
+class CompleteFilter {
+    name = "completeFilter";
+    htmlIdentifier = "complete-filter-btn";
+    active = false;
+    filter = (toDoItem) => {
+        return this.active ? !toDoItem.Complete : true
     }
+}
 
+export const init = () => {
+    registerFilters();
+
+    generateList();
+}
+
+const getFilteredItems = () => {
+    const filteredResults = toDoItems.filter(item => {
+        // only do active filters?
+        return filters.every(filter => filter.filter(item));
+    })
+    return filteredResults;
+}
+
+const generateList = () => {
     const itemList = document.getElementById('item-list');
 
-    const generateList = () => {
-        itemList.innerHTML = '';
-        const filteredItems = getFilteredItems();
-        filteredItems.forEach((item) => {
-            const newListItem = document.createElement('li');
-            newListItem.innerHTML = item.Name;
-            itemList.appendChild(newListItem);
-        });
-    }
+    itemList.innerHTML = '';
+    const filteredItems = getFilteredItems();
+    filteredItems.forEach((item) => {
+        const newListItem = document.createElement('li');
+        newListItem.innerHTML = item.Name;
+        itemList.appendChild(newListItem);
+    });
+}
 
-    return {
-        generateList: generateList
-    }
+const highPriorityFilter = new HighPriorityFilter();
+const completeFilter = new CompleteFilter();
+
+const filters = [highPriorityFilter, completeFilter]
+
+const registerFilters = () => {
+    filters.forEach((filter) => {
+        const filterBtn = document.getElementById(filter.htmlIdentifier);
+        filterBtn.addEventListener('click', () => filterClick(filter));
+    })
+}
+
+const filterClick = (filter) => {
+    filter.active = !filter.active;
+    generateList();
 }
