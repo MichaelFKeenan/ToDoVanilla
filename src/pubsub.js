47fc4befe215482c70a659f5ecced4eb9c684e6f
Michael Keenan <Michael.Keenan@howdens.com>
2020-02-26 11:41:03 +0000

    add unsubscribe method to pubsub

--- src/pubsub.js
+++ src/pubsub.js
@@ -1,14 +1,23 @@
-let subscribers = {};
+let subscribers = {
+    "something": () => true
+};
 export default {
     publish(event, data) {
         if (!subscribers[event]) return;
         subscribers[event].forEach(subscriberCallback =>
             subscriberCallback(data));
     },
+    //this allows multiple callbacks to be assigned to a single event... is that correct?
     subscribe(event, callback) {
         if (!subscribers[event]) {
             subscribers[event] = [];
         }
         subscribers[event].push(callback);
+    },
+    unsubscribe(event){
+        if (!subscribers[event]) {
+            return;
+        }
+        delete subscribers[event];
     }
 };
