4fa01480aacb2b7b27e919e05c5e85441ff267b3
KikeyMeenan <mikemk309@googlemail.com>
2020-04-06 11:39:29 +0100

    categories as dropdown

--- src/createItem.js
+++ src/createItem.js
@@ -1,43 +1,67 @@
 import { addItem } from './itemsService.js'
+import { getAllCategories } from './categoryService.js'
+
+let submitBtn;
+let priorityInput;
+let priorityInputValueDisplay;
+let categorySelect;
 
 export const init = async () => {
-    const submitBtn = document.getElementById('item-submit');
-    const priorityInput = document.getElementById('item-priority');
-    const priorityInputValueDisplay = document.getElementById('item-priority-value-display');
+    submitBtn = document.getElementById('item-submit');
+    priorityInput = document.getElementById('item-priority');
+    priorityInputValueDisplay = document.getElementById('item-priority-value-display');
+    categorySelect = document.getElementById('category-select');
     
-    const updateItemPriorityValueDisplay = () => {
-        priorityInputValueDisplay.innerHTML = priorityInput.value;
-    }
-
     updateItemPriorityValueDisplay();
     
     priorityInput.addEventListener('change', updateItemPriorityValueDisplay)
 
-    submitBtn.addEventListener('click', async () => {
-        submitBtn.disabled = true;
-        //validation?
-        const nameValue = document.getElementById('item-name').value;
-        const priorityValue = priorityInput.value;
-        const categoryValue = document.getElementById('item-category').value;
-        const descriptionValue = document.getElementById('item-description').value;
-        const newItem = {
-            Id: null,
-            Name: nameValue,
-            Complete: false,
-            Priority: Number(priorityValue),
-            CategoryId: Number(categoryValue),
-            Description: descriptionValue
-        }
+    submitBtn.addEventListener('click', submitForm)
 
-        //find a much nicer way to do this!
-        var res = await addItem(newItem);
-        if(res.status == 200)
-        {
-            window.location.replace("/")
-        }
-        else {
-            alert('aaaaah an error! tell michael!')
-        };
-        submitBtn.disabled = false;
-    })
+    await populateCategoriesDropdown();
+}
+
+const populateCategoriesDropdown = async () => {
+    const categories = await getAllCategories();
+
+    categories.forEach(option => {
+      const newOptionEl = document.createElement('option');
+      newOptionEl.innerHTML = option.Name;
+      newOptionEl.value = option.Id;
+      categorySelect.appendChild(newOptionEl);
+    });
+}
+
+const updateItemPriorityValueDisplay = () => {
+    priorityInputValueDisplay.innerHTML = priorityInput.value;
+}
+
+const submitForm = async () => {
+    submitBtn.disabled = true;
+    //validation?
+    const nameValue = document.getElementById('item-name').value;
+    const priorityValue = priorityInput.value;
+    const categorySelectValue = categorySelect.value;
+    const descriptionValue = document.getElementById('item-description').value;
+    const newItem = {
+        Id: null,
+        Name: nameValue,
+        Complete: false,
+        Priority: Number(priorityValue),
+        CategoryId: Number(categorySelectValue),
+        Description: descriptionValue
+    }
+
+    console.log(newItem)
+
+    //find a much nicer way to do this!
+    var res = await addItem(newItem);
+    if(res.status == 200)
+    {
+        window.location.replace("/")
+    }
+    else {
+        alert('aaaaah an error! tell michael!')
+    };
+    submitBtn.disabled = false;
 }
